692ae8f933e4eb3882d71899e6ee7f5c9f5f4b59
Ranjot Singh <ranjot147@gmail.com>
2016-02-05 21:50:10 -0800

    init

---
+++ content.js
@@ -0,0 +1,18 @@
+﻿var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/😀/gi, '<img src=\"http://emojipedia-us.s3.amazonaws.com/cache/49/b8/49b846579cf59834716c1b0a73fa166c.png\" />');
+			
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
