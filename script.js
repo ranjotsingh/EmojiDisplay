25490bee1fb14af0736c036ed2d17617702a1f53
Ranjot Singh <ranjot147@gmail.com>
2016-03-20 13:40:18 -0700

    organization

---
+++ script.js
@@ -0,0 +1,113 @@
+﻿var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+		
+		showEmojis();
+    }
+}
+
+function showEmojis() {
+	addEmoji('😀', "https://emojipedia-us.s3.amazonaws.com/cache/49/b8/49b846579cf59834716c1b0a73fa166c.png");
+	addEmoji('😁', "https://emojipedia-us.s3.amazonaws.com/cache/93/27/9327b222795650ef891a38440aec0de7.png");
+	addEmoji('😂', "https://emojipedia-us.s3.amazonaws.com/cache/17/47/17474d3a52fd7efdada7b5896b5c5906.png");
+	addEmoji('😃', "https://emojipedia-us.s3.amazonaws.com/cache/a9/48/a948113eb86d6ead15c4626fd88fb440.png");
+	addEmoji('😄', "https://emojipedia-us.s3.amazonaws.com/cache/a0/38/a038e6d3f342253c5ea3c057fe37b41f.png");
+	addEmoji('😅', "https://emojipedia-us.s3.amazonaws.com/cache/28/10/281085c6e7d7d6059b8586abcb084621.png");
+	addEmoji('😆', "https://emojipedia-us.s3.amazonaws.com/cache/fa/c1/fac171681f0df5976a66797e84b22946.png");
+	addEmoji('😉', "https://emojipedia-us.s3.amazonaws.com/cache/5d/46/5d460ed78992a28f0ea9d7a6b7841f91.png");
+	addEmoji('😊', "https://emojipedia-us.s3.amazonaws.com/cache/4a/bc/4abc340cf5d893ff4bf6ebc17b29c221.png");
+	addEmoji('😋', "https://emojipedia-us.s3.amazonaws.com/cache/56/6c/566c9bb1eb9cee4ad2a8e267e46cff7d.png");
+	addEmoji('😎', "https://emojipedia-us.s3.amazonaws.com/cache/80/9b/809b8e422f88d1b45305897877109d1c.png");
+	addEmoji('😍', "https://emojipedia-us.s3.amazonaws.com/cache/6f/aa/6faaa08700cb0741e8ba3c5fed121449.png");
+	addEmoji('😘', "https://emojipedia-us.s3.amazonaws.com/cache/c6/74/c674d5746f3f669964324fc1faf8e415.png");
+	addEmoji('😗', "https://emojipedia-us.s3.amazonaws.com/cache/13/0a/130adeac887ca37241e2dcfa3eed24f1.png");
+	addEmoji('😙', "https://emojipedia-us.s3.amazonaws.com/cache/5f/6a/5f6ae8c3e48cd60136d1f55b6adcda9c.png");
+	addEmoji('😚', "https://emojipedia-us.s3.amazonaws.com/cache/48/f4/48f4f2bbd8206cb07a9bc576539bb4d2.png");
+	addEmoji('☺', "https://emojipedia-us.s3.amazonaws.com/cache/a3/df/a3dfd7946b3a59f3cac159d0cdfd089d.png");
+	addEmoji('🙂', "https://emojipedia-us.s3.amazonaws.com/cache/9f/64/9f64f990f3904ef6f88a06d4054ab8fc.png");
+	addEmoji('🤗', "https://emojipedia-us.s3.amazonaws.com/cache/80/dd/80dd26f1aa392c86c98f2de2b51a62e3.png");
+	addEmoji('😇', "https://emojipedia-us.s3.amazonaws.com/cache/33/92/3392edca1d4b41a67511b2595585d35d.png");
+	addEmoji('🤔', "https://emojipedia-us.s3.amazonaws.com/cache/db/d5/dbd57bcbb3fff7245025a39061012200.png");
+	addEmoji('😐', "https://emojipedia-us.s3.amazonaws.com/cache/68/90/689096bdc3bb25278d80d3aae4866c2e.png");
+	addEmoji('😑', "https://emojipedia-us.s3.amazonaws.com/cache/d5/3d/d53d304bebc2c9b1f3b911a17d4c6a67.png");
+	addEmoji('😶', "https://emojipedia-us.s3.amazonaws.com/cache/96/1b/961bd696d15dde1061d16bc78e4bff55.png");
+	addEmoji('🙄', "https://emojipedia-us.s3.amazonaws.com/cache/41/af/41af1c0f05f9c6e1b0b4f789214454d9.png");
+	addEmoji('😏', "https://emojipedia-us.s3.amazonaws.com/cache/3b/57/3b57c2efe844d5cae0f9982a264fa11a.png");
+	addEmoji('😣', "https://emojipedia-us.s3.amazonaws.com/cache/31/03/3103485590734b493e03031d9d2a359e.png");
+	addEmoji('😥', "https://emojipedia-us.s3.amazonaws.com/cache/de/a0/dea097989107ff735a28be71b32f35d6.png");
+	addEmoji('😮', "https://emojipedia-us.s3.amazonaws.com/cache/dc/38/dc3892a20737ae1a2b14f91c1ed0d47f.png");
+	addEmoji('🤐', "https://emojipedia-us.s3.amazonaws.com/cache/d3/aa/d3aa92eeb2e4836e00d9245479bccaf9.png");
+	addEmoji('😯', "https://emojipedia-us.s3.amazonaws.com/cache/87/ba/87bafe0dd6b9d3bcaf7f8b925c8540b5.png");
+	addEmoji('😪', "https://emojipedia-us.s3.amazonaws.com/cache/b1/63/b163096009c1b90f19864be8a72c3086.png");
+	addEmoji('😫', "https://emojipedia-us.s3.amazonaws.com/cache/7c/fe/7cfe744ca29a6a28cb0ab3ea52fefd0b.png");
+	addEmoji('😴', "https://emojipedia-us.s3.amazonaws.com/cache/4d/1f/4d1fd387de3dc0c882b10292a3cc1cbd.png");
+	addEmoji('😌', "https://emojipedia-us.s3.amazonaws.com/cache/9c/29/9c295f2c7650ccd2c29008c3534b689a.png");
+	addEmoji('🤓', "https://emojipedia-us.s3.amazonaws.com/cache/65/bc/65bcd2590f982f6c95de78664861ed58.png");
+	addEmoji('😛', "https://emojipedia-us.s3.amazonaws.com/cache/03/0c/030c859333952ba3230d5b9a0db9e508.png");
+	addEmoji('😜', "https://emojipedia-us.s3.amazonaws.com/cache/78/69/78692604bb852884226c91b6b0f3891a.png");
+	addEmoji('😝', "https://emojipedia-us.s3.amazonaws.com/cache/63/ea/63ea1f5cf0860234b25183d65afc7cc8.png");
+	addEmoji('☹', "https://emojipedia-us.s3.amazonaws.com/cache/e9/90/e9903d3e0dc2042d5a4b473485ecc5de.png");
+	addEmoji('🙁', "https://emojipedia-us.s3.amazonaws.com/cache/4e/bb/4ebb849a2fe5cb7f2f5945f404d1857c.png");
+	addEmoji('😒', "https://emojipedia-us.s3.amazonaws.com/cache/f6/87/f6878539136752df6f59bbd305997f55.png");
+	addEmoji('😓', "https://emojipedia-us.s3.amazonaws.com/cache/1c/91/1c9154acdfea1bb17c5ded1451c45d1b.png");
+	addEmoji('😔', "https://emojipedia-us.s3.amazonaws.com/cache/8d/5d/8d5d28ec666d382134189cf8e44faa9b.png");
+	addEmoji('😕', "https://emojipedia-us.s3.amazonaws.com/cache/89/9b/899be708bc2317ff5c15b2f87bee1c2e.png");
+	addEmoji('😖', "https://emojipedia-us.s3.amazonaws.com/cache/19/f1/19f12fb3c9886505d6578af8d13b8aea.png");
+	addEmoji('🙃', "https://emojipedia-us.s3.amazonaws.com/cache/ab/c9/abc96c46b822fe45ab344a86e054315e.png");
+	addEmoji('😷', "https://emojipedia-us.s3.amazonaws.com/cache/26/fb/26fb73780cb84e6d32a5f05e259d2a05.png");
+	addEmoji('🤒', "https://emojipedia-us.s3.amazonaws.com/cache/9d/3d/9d3d73bcedc19f26a8a0aea45d07adf7.png");
+	addEmoji('🤕', "https://emojipedia-us.s3.amazonaws.com/cache/e7/7b/e77be89961d1904d95a863b99419bdfd.png");
+	addEmoji('🤑', "https://emojipedia-us.s3.amazonaws.com/cache/54/79/54793fc6cc0899235b4d563d8f1184a8.png");
+	addEmoji('😲', "https://emojipedia-us.s3.amazonaws.com/cache/65/00/65002a402600b5cd98fc49af7d2a01e4.png");
+	addEmoji('😞', "https://emojipedia-us.s3.amazonaws.com/cache/ee/ff/eeffbeddcf47544b1458bd91d0c7b06d.png");
+	addEmoji('😟', "https://emojipedia-us.s3.amazonaws.com/cache/17/56/175613c61ad30455da9c4869774013aa.png");
+	addEmoji('😤', "https://emojipedia-us.s3.amazonaws.com/cache/bb/11/bb1132a64cb645e3f8e95df382ecfd65.png");
+	addEmoji('😢', "https://emojipedia-us.s3.amazonaws.com/cache/bc/6f/bc6f71bfca5a45797cea82ac6f6b9864.png");
+	addEmoji('😭', "https://emojipedia-us.s3.amazonaws.com/cache/77/4d/774daba0c37384f2edb4d931f898b0dc.png");
+	addEmoji('😦', "https://emojipedia-us.s3.amazonaws.com/cache/cc/d2/ccd20e495df546701e621b528b6760db.png");
+	addEmoji('😧', "https://emojipedia-us.s3.amazonaws.com/cache/e5/80/e580e6bb5fac84f879b540ed9648ddda.png");
+	addEmoji('😨', "https://emojipedia-us.s3.amazonaws.com/cache/f7/83/f78375aaf83325460177ad46142077cf.png");
+	addEmoji('😩', "https://emojipedia-us.s3.amazonaws.com/cache/2f/c7/2fc7477cefc965ad6c75b2adf29edad3.png");
+	addEmoji('😬', "https://emojipedia-us.s3.amazonaws.com/cache/f3/99/f399252b5bf7c2203817b095db4b32bf.png");
+	addEmoji('😰', "https://emojipedia-us.s3.amazonaws.com/cache/a1/8e/a18e59e5c8da265343965dafe32acc49.png");
+	addEmoji('😱', "https://emojipedia-us.s3.amazonaws.com/cache/5f/d6/5fd6191df3085e00535a8d295dc49b1b.png");
+	addEmoji('😳', "https://emojipedia-us.s3.amazonaws.com/cache/00/00/000087279dcb44fb2fcd1be2848bf662.png");
+	addEmoji('😵', "https://emojipedia-us.s3.amazonaws.com/cache/53/ef/53ef1bf4fecf1fdfb170f91f8ae9e186.png");
+	addEmoji('😡', "https://emojipedia-us.s3.amazonaws.com/cache/eb/73/eb73219f12956344a6a267d23ce21264.png");
+	addEmoji('😠', "https://emojipedia-us.s3.amazonaws.com/cache/e7/3e/e73ed4d57793a56fcb7ff8fb49ef45bc.png");
+	addEmoji('👿', "https://emojipedia-us.s3.amazonaws.com/cache/18/1b/181bd1057c8e9b3755c0d7c38892da3c.png");
+	addEmoji('😈', "https://emojipedia-us.s3.amazonaws.com/cache/ec/bd/ecbd91e2065e533046a26829a123bbb2.png");
+	
+	addEmoji('💯', "https://emojipedia-us.s3.amazonaws.com/cache/42/6d/426d5d9155c35c44e66980c4187f6bab.png");
+}
+
+function addEmoji(ASCII, imgURL) {
+	if (node.nodeType === Node.TEXT_NODE) {
+		var text = node.nodeValue;
+		var part = splitWithTail(text, ASCII);
+		var regex = new RegExp(ASCII, "g");
+		var old = node.parentElement;
+		
+		var replacedText = text.replace(regex, img);
+		if (replacedText !== text && old !== null) {
+			var img = new Image(20, 20);
+			img.src = imgURL;
+			old.insertBefore(document.createTextNode(part[0]), node);
+			old.insertBefore(img, node);
+			old.insertBefore(document.createTextNode(part[1]), node);
+			old.removeChild(node);
+		}
+	}
+}
+
+function splitWithTail(str, delim) {
+	var parts = str.split(delim);
+	var tail = parts.slice(1).join(delim);
+	var result = parts.slice(0, 1);
+	result.push(tail);
+	return result;
+}
