be1bced42a254f162bd9766b98618c1fca792b12
ranjot147@gmail.com <ranjot147@gmail.com>
2018-11-15 14:00:25 -0800

    optimization

--- content.js
+++ content.js
@@ -1,33 +1,33 @@
 ﻿(function() {
-  let emojiList;
+  let emojiMap;
   fetch(chrome.runtime.getURL("res.json"))
     .then(response => response.json())
     .then(response => {
-      emojiList = new Map(Object.entries(response));
-      emojiList = new Map([...emojiList].filter((k, v) => k[0].length <= 2));
+      emojiMap = Object.entries(response);
     })
     .then(() => main());
 
   function main() {
     const nodes = document.querySelectorAll("p,span,h1,h2,h3,h4,h5,h6");
-
-    function mainLoop() {
-      for (const node of nodes) {
-        for (const [ASCII, imgURL] of emojiList.entries()) {
+    for (const node of nodes) {
+      let i = 0;
+      (function mainLoop() {
+        if (i < emojiMap.length) {
+          const [ASCII, imgURL] = emojiMap[i];
           if (
             node.parentElement &&
             node.innerText &&
-            node.innerText.includes(ASCII) &&
-            !node.innerHTML.includes('{"')
+            node.innerText.includes(ASCII)
           ) {
             node.innerHTML = node.innerHTML.replace(
               new RegExp(ASCII, "g"),
               `<img src="${chrome.runtime.getURL(imgURL)}" width=15 height=15>`
             );
           }
+          ++i;
+          setTimeout(mainLoop, 0);
         }
-      }
+      })();
     }
-    setTimeout(mainLoop, 0);
   }
 })();
