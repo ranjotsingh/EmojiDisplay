75b22a7b43ccd5800f828e4d3a76818b470b5d6e
ranjot147@gmail.com <ranjot147@gmail.com>
2018-11-14 21:22:53 -0800

    more emojis supported, performance fix

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-This is a Chrome extension, which replaces text versions of the emojis into images (for Operating Systems that do not automatically support this feature).
+# EmojiDisplay
+
+EmojiDisplay is a Chrome extension, which replaces text versions of the emojis into images (for operating systems that do not automatically support this feature).
 
 e.g. Special characters 😀, 😁, 😂, 😃, etc. are replaced with graphics of Apple Emojis.
 
-Image Credits: Apple and Emojipedia
+## Installation
+You can download the Google Chrome extension [here](https://chrome.google.com/webstore/detail/emojidisplay/doomojimaojclkcipjldpmdbiedgaoop).
+
+## Credits
+- Apple
+- Emojipedia
--- content.js
+++ content.js
@@ -1,6 +1,33 @@
-﻿var s = document.createElement('script');
-s.src = chrome.extension.getURL('script.js');
-s.onload = function() {
-    this.parentNode.removeChild(this);
-};
-(document.head || document.documentElement).appendChild(s);
+﻿(function() {
+  let emojiList;
+  fetch(chrome.runtime.getURL("res.json"))
+    .then(response => response.json())
+    .then(response => {
+      emojiList = new Map(Object.entries(response));
+      emojiList = new Map([...emojiList].filter((k, v) => k[0].length <= 2));
+    })
+    .then(() => main());
+
+  function main() {
+    const nodes = document.querySelectorAll("p,span,h1,h2,h3,h4,h5,h6");
+
+    function mainLoop() {
+      for (const node of nodes) {
+        for (const [ASCII, imgURL] of emojiList.entries()) {
+          if (
+            node.parentElement &&
+            node.innerText &&
+            node.innerText.includes(ASCII) &&
+            !node.innerHTML.includes('{"')
+          ) {
+            node.innerHTML = node.innerHTML.replace(
+              new RegExp(ASCII, "g"),
+              `<img src="${chrome.runtime.getURL(imgURL)}" width=15 height=15>`
+            );
+          }
+        }
+      }
+    }
+    setTimeout(mainLoop, 0);
+  }
+})();
